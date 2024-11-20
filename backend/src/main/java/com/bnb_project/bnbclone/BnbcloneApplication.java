6def2cf46549821491c45b3962901049cea71d57
OvidiuAndrei98 <andrei.penica@gmail.com>
2024-11-21 00:28:00 +0200

    initial project structure

---
+++ backend/src/main/java/com/bnb_project/bnbclone/BnbcloneApplication.java
@@ -0,0 +1,13 @@
+package com.bnb_project.bnbclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BnbcloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BnbcloneApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bnb_project/bnbclone/BnbcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnb_project.bnbclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BnbcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
