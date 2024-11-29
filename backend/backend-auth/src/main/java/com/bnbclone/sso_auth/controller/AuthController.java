85e1bdb64daf4741eb9bc567afd58752ff4b9477
OvidiuAndrei98 <andrei.penica@gmail.com>
2024-11-29 16:38:05 +0200

    fixed auth

--- backend/backend-auth/src/main/java/com/bnbclone/sso_auth/controller/AuthController.java
+++ backend/backend-auth/src/main/java/com/bnbclone/sso_auth/controller/AuthController.java
@@ -8,12 +8,12 @@ import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2Aut
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class AuthController {
 
     @Autowired
