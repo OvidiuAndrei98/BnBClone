239b65fedd8c09733ba3e006fd5b7206ff3a5a6b
OvidiuAndrei98 <andrei.penica@gmail.com>
2024-12-02 03:41:28 +0200

    feat(general): changed login page to work with google sso

--- frontend/src/core/AuthenticationBoundary.tsx
+++ frontend/src/core/AuthenticationBoundary.tsx
@@ -10,28 +10,28 @@ import { redirect, useMatch } from 'react-router';
  * Contains constants which describe the authentication state of the current session.
  */
 enum AuthenticationState {
-    /**
-     * Indicates that the authentication state has not been determined yet.
-     */
-    Unknown,
+  /**
+   * Indicates that the authentication state has not been determined yet.
+   */
+  Unknown,
 
-    /**
-     * Indicates that the current session is not authenticated.
-     */
-    Unauthenticated,
+  /**
+   * Indicates that the current session is not authenticated.
+   */
+  Unauthenticated,
 
-    /**
-     * Indicates that the current session is authenticated.
-     */
-    Authenticated,
+  /**
+   * Indicates that the current session is authenticated.
+   */
+  Authenticated,
 }
 
 /**
  * A context that contains information about the currently authenticated user.
  */
 export const AuthenticationContext = React.createContext({
-    authenticationState: AuthenticationState.Unknown,
-    userDetails: {} as TokenValues,
+  authenticationState: AuthenticationState.Unknown,
+  userDetails: {} as TokenValues,
 });
 
 /**
@@ -44,44 +44,44 @@ const AuthenticationTokenKey = 'auth_token';
  * a token is still valid before performing a request.
  */
 interface TokenValues {
-    /**
-     * The timestamp when this token will expire, in seconds.
-     */
-    exp: number;
+  /**
+   * The timestamp when this token will expire, in seconds.
+   */
+  exp: number;
 
-    /**
-     * The timestamp when this token was issued, in seconds.
-     */
-    iat: number;
+  /**
+   * The timestamp when this token was issued, in seconds.
+   */
+  iat: number;
 
-    /**
-     * The email address of the user for which the token was issued.
-     */
-    email: string;
+  /**
+   * The email address of the user for which the token was issued.
+   */
+  email: string;
 
-    /**
-     * The ID of the system for which the token was issued.
-     */
-    system_id: string;
+  /**
+   * The ID of the system for which the token was issued.
+   */
+  system_id: string;
 
-    /**
-     * The ID of the tenant to which the user belongs.
-     */
-    tenant_id: string;
+  /**
+   * The ID of the tenant to which the user belongs.
+   */
+  tenant_id: string;
 
-    /**
-     * The ID of the user for which the token was issued.
-     */
-    user_id: string;
+  /**
+   * The ID of the user for which the token was issued.
+   */
+  user_id: string;
 
-    /**
-     * The name of the user for which the token was issued.
-     */
-    user_name: string;
+  /**
+   * The name of the user for which the token was issued.
+   */
+  user_name: string;
 
-    picture: string;
+  picture: string;
 
-    // TODO: To be completed after decoding a token
+  // TODO: To be completed after decoding a token
 }
 
 /**
@@ -91,24 +91,24 @@ interface TokenValues {
  *                      be decoded, `undefined` otherwise.
  */
 function DecodeJWT(token: string): TokenValues | undefined {
-    // NOTE: Code from https://stackoverflow.com/questions/38552003/how-to-decode-jwt-token-in-javascript-without-using-a-library
-    try {
-        const base64URL = token.split('.')[1];
-        const base64Content = base64URL.replace(/-/g, '+').replace(/_/g, '/');
-        const JSONPayload = decodeURIComponent(
-            window
-                .atob(base64Content)
-                .split('')
-                .map(function (c) {
-                    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-                })
-                .join(''),
-        );
+  // NOTE: Code from https://stackoverflow.com/questions/38552003/how-to-decode-jwt-token-in-javascript-without-using-a-library
+  try {
+    const base64URL = token.split('.')[1];
+    const base64Content = base64URL.replace(/-/g, '+').replace(/_/g, '/');
+    const JSONPayload = decodeURIComponent(
+      window
+        .atob(base64Content)
+        .split('')
+        .map(function (c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join(''),
+    );
 
-        return JSON.parse(JSONPayload) as TokenValues;
-    } catch (e) {
-        console.error(`Could not decode token.`, e);
-    }
+    return JSON.parse(JSONPayload) as TokenValues;
+  } catch (e) {
+    console.error(`Could not decode token.`, e);
+  }
 }
 
 /**
@@ -120,98 +120,99 @@ function DecodeJWT(token: string): TokenValues | undefined {
  * @returns             A react element.
  */
 export function AuthenticationBoundary(props: { children?: ReactNode }) {
-    const [authenticationState, setAuthenticationState] = useState(AuthenticationState.Unknown);
-    const [token, setToken] = useState('');
-    const [tokenValues, setTokenValues] = useState<TokenValues>();
-    const queryParameters = new URLSearchParams(window.location.search);
+  const [authenticationState, setAuthenticationState] = useState(AuthenticationState.Unknown);
+  const [token, setToken] = useState('');
+  const [tokenValues, setTokenValues] = useState<TokenValues>();
+  const queryParameters = new URLSearchParams(window.location.search);
 
-    // Displays a loading indicator while a login request is in progress
-    const [isLoggingIn, setLoggingIn] = useState(false);
+  // Displays a loading indicator while a login request is in progress
+  const [isLoggingIn, setLoggingIn] = useState(false);
 
-    useEffect(() => {
-        let token = queryParameters.get('token');
-        setToken(token!);
-        try {
-            const tkn = localStorage.getItem(AuthenticationTokenKey);
+  useEffect(() => {
+    let token = queryParameters.get('token');
+    setToken(token!);
+    try {
+      const tkn = localStorage.getItem(AuthenticationTokenKey);
 
-            if (!tkn) {
-                token = queryParameters.get('token');
-            }
-            token = tkn ? JSON.parse(tkn) : undefined;
-        } catch (e) {
-            // Assume the token is not set if an error occurs while reading
-            // the local storage (e.g. safari private mode)
-            console.error(`Could not read the token from local storage. `, e);
-        }
+      if (!tkn) {
+        token = queryParameters.get('token');
+      }
+      token = tkn ? JSON.parse(tkn) : undefined;
+    } catch (e) {
+      // Assume the token is not set if an error occurs while reading
+      // the local storage (e.g. safari private mode)
+      console.error(`Could not read the token from local storage. `, e);
+    }
 
-        if (token) {
-            // If the token is set, set it, which triggers an effect that verifies it
-            setToken(token);
+    if (token) {
+      // If the token is set, set it, which triggers an effect that verifies it
+      setToken(token);
+    } else {
+      // Otherwise assume not authenticated
+      setAuthenticationState(AuthenticationState.Unauthenticated);
+    }
+  }, []);
+
+  useEffect(() => {
+    // When the token changes, parse it and verify its validity
+    if (token) {
+      const tokenValues = DecodeJWT(token);
+      if (tokenValues) {
+        setTokenValues(tokenValues);
+        if (tokenValues.exp * 1000 > Date.now()) {
+          // If the token is valid, set the authentication state as authenticated
+          setAuthenticationState(AuthenticationState.Authenticated);
+
+          try {
+            localStorage.setItem('auth_token', JSON.stringify(token));
+            queryParameters.delete('token');
+          } catch (e) {
+            console.error('Could not persist authorization token.', e);
+            // If the token can't be stored, just log an error; the current session
+            // will work, but the user will be asked to reauthenticate when reloading
+          }
         } else {
-            // Otherwise assume not authenticated
-            setAuthenticationState(AuthenticationState.Unauthenticated);
+          // Otherwise require users to log in again
+          setAuthenticationState(AuthenticationState.Unauthenticated);
         }
-    }, []);
-
-    useEffect(() => {
-        // When the token changes, parse it and verify its validity
-        if (token) {
-            const tokenValues = DecodeJWT(token);
-            if (tokenValues) {
-                setTokenValues(tokenValues);
-                if (tokenValues.exp * 1000 > Date.now()) {
-                    // If the token is valid, set the authentication state as authenticated
-                    setAuthenticationState(AuthenticationState.Authenticated);
-
-                    try {
-                        localStorage.setItem('auth_token', JSON.stringify(token));
-                        queryParameters.delete('token');
-                    } catch (e) {
-                        console.error('Could not persist authorization token.', e);
-                        // If the token can't be stored, just log an error; the current session
-                        // will work, but the user will be asked to reauthenticate when reloading
-                    }
-                } else {
-                    // Otherwise require users to log in again
-                    setAuthenticationState(AuthenticationState.Unauthenticated);
-                }
-            }
-        }
-    }, [token]);
-
-    /**
-     * Redirects to the SSO login page to obtain the authorization token.
-     */
-    async function login() {
-        setLoggingIn(true);
-        // Obtain the URL to the SSO authentication page and redirect to it
-        try {
-            await authenticate('/sso/auth');
-            // Store the temporary request token and use it after the redirect to obtain an authentication token
-        } catch (e) {
-            alert('login failed');
-            setLoggingIn(false);
-        }
+      }
     }
+  }, [token]);
 
-    switch (authenticationState) {
-        case AuthenticationState.Unknown:
-        default:
-        // return <Skeleton />;
-        case AuthenticationState.Unauthenticated:
-            return <Login onLogin={login} loggingIn={isLoggingIn} />;
-        case AuthenticationState.Authenticated:
-            // For authenticated contexts, just render the children normally
-            return (
-                <AuthenticationContext.Provider
-                    value={{
-                        authenticationState,
-                        // NOTE: When state is authenticated, tokenValues is non-null
-                        userDetails: tokenValues!,
-                    }}
-                >
-                    {props.children ?? null}
-                </AuthenticationContext.Provider>
-            );
+  /**
+   * Redirects to the SSO login page to obtain the authorization token.
+   * @param provider authorization provider
+   */
+  async function login(provider: string) {
+    setLoggingIn(true);
+    // Obtain the URL to the SSO authentication page and redirect to it
+    try {
+      await authenticate(`/sso/auth?provider=${provider}`);
+      // Store the temporary request token and use it after the redirect to obtain an authentication token
+    } catch (e) {
+      alert('login failed');
+      setLoggingIn(false);
     }
+  }
+
+  switch (authenticationState) {
+    case AuthenticationState.Unknown:
+    default:
+    // return <Skeleton />;
+    case AuthenticationState.Unauthenticated:
+      return <Login onLogin={login} loggingIn={isLoggingIn} />;
+    case AuthenticationState.Authenticated:
+      // For authenticated contexts, just render the children normally
+      return (
+        <AuthenticationContext.Provider
+          value={{
+            authenticationState,
+            // NOTE: When state is authenticated, tokenValues is non-null
+            userDetails: tokenValues!,
+          }}
+        >
+          {props.children ?? null}
+        </AuthenticationContext.Provider>
+      );
+  }
 }
